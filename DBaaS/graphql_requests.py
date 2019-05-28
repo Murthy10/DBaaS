4a4b71e52a6444452b3ee94af22ce3fff3160b21
Samuel Kurath <samuel.kurath@gmail.com>
2019-05-28 15:00:47 +0200

    updated readme.md

--- DBaaS/graphql_requests.py
+++ DBaaS/graphql_requests.py
@@ -3,7 +3,6 @@ import os
 import json
 
 import requests
-import numpy as np
 import pandas as pd
 
 from DBaaS import sakila_graphql
@@ -12,19 +11,21 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 URL = "http://localhost:5001/graphql"
 
-QUERIES = [(func, name) for name, func in inspect.getmembers(sakila_graphql) if 'query' in name]
+SAKILA_QUERIES = [(func, name) for name, func in inspect.getmembers(sakila_graphql) if 'query' in name]
 
 
 def run():
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     data_frame = pd.DataFrame(columns=['Query', 'Time', 'Records'])
-    for query, name in QUERIES:
+    print('SAKILA:')
+    for query, name in SAKILA_QUERIES:
         data = dict(query=query)
         response = requests.post(URL, data=json.dumps(data), headers=headers)
         data_frame.loc[-1] = [name, response.elapsed.total_seconds(), length(response)]
         data_frame.index = data_frame.index + 1
         data_frame = data_frame.sort_index()
     print(data_frame)
+    print()
 
 
 def length(response):
--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # DBaaS
+To use this repo you have to install docker and docker-compose on your system.
+Further, python3.6 and pip is required.
+
+Run the containers:
+```bash
+docker-compose build
+docker-compose up
+```
 
 ## postgraphile
-Plugins:
-  * postgraphile-plugin-connection-filter
-  * @graphile/postgis
-  * @graphile-contrib/pg-order-by-related
-  * @graphile/pg-aggreates
+Run: (previously, install the dependencies mentioned in the requirements.txt)
+```bash
+python3 graphql_requests.py
+```
+
 
 ### Notes
   * ORDER BY multiple columns -> @graphile-contrib/pg-order-by-related
@@ -13,15 +21,8 @@ Plugins:
   * SUM not possible -> Views (wip @graphile/pg-aggreates)
   * Nested Queries not possible -> View (7a..)
 
-
-##
-```bash
-{
-  allActors(filter: {firstName: {includes: "a"}}) {
-    nodes {
-      firstName
-      lastName
-    }
-  }
-}
-```
+Used plugins:
+  * postgraphile-plugin-connection-filter
+  * @graphile/postgis
+  * @graphile-contrib/pg-order-by-related
+  * @graphile/pg-aggreates
